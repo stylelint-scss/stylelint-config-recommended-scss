1e40cc1e1a288cc01aeeeccd8a0f72968bc9807a
Richard Hallows <jeddy3@users.noreply.github.com>
2021-10-21 17:11:03 +0100

    Prepare 5.0.0 (#84)

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,104 +1,72 @@
-"use strict";
+'use strict';
 
-const config = require("../");
-const fs = require("fs");
-const stylelint = require("stylelint");
+const config = require('../');
+const fs = require('fs');
+const stylelint = require('stylelint');
 
-const validCss = fs.readFileSync("./__tests__/css-valid.css", "utf-8");
-const invalidCss = fs.readFileSync("./__tests__/css-invalid.css", "utf-8");
-const validScss = fs.readFileSync("./__tests__/scss-valid.scss", "utf-8");
+const validScss = fs.readFileSync('./__tests__/valid.scss', 'utf-8');
+const invalidScss = fs.readFileSync('./__tests__/invalid.scss', 'utf-8');
 
-describe("flags no warnings with valid css", () => {
-  let result;
+describe('flags no warnings with valid scss', () => {
+	let result;
 
-  beforeEach(() => {
-    result = stylelint.lint({
-      code: validCss,
-      config
-    });
-  });
+	beforeEach(() => {
+		result = stylelint.lint({
+			code: validScss,
+			config,
+		});
+	});
 
-  it("did not error", () => {
-    return result.then(data => expect(data.errored).toBeFalsy());
-  });
+	it('did not error', () => {
+		return result.then((data) => expect(data.errored).toBeFalsy());
+	});
 
-  it("flags no warnings", () => {
-    return result.then(data =>
-      expect(data.results[0].warnings).toHaveLength(0)
-    );
-  });
+	it('flags no warnings', () => {
+		return result.then((data) => expect(data.results[0].warnings).toHaveLength(0));
+	});
 });
 
-describe("flags no warnings with valid scss", () => {
-  let result;
+describe('flags warnings with invalid scss', () => {
+	let result;
 
-  beforeEach(() => {
-    result = stylelint.lint({
-      code: validScss,
-      config,
-    });
-  });
+	beforeEach(() => {
+		result = stylelint.lint({
+			code: invalidScss,
+			config,
+		});
+	});
 
-  it("did not error", () => {
-    return result.then(data => expect(data.errored).toBeFalsy());
-  });
+	it('did error', () => {
+		return result.then((data) => expect(data.errored).toBeTruthy());
+	});
 
-  it("flags no warnings", () => {
-    return result.then(data =>
-      expect(data.results[0].warnings).toHaveLength(0)
-    );
-  });
-});
+	it('flags one warning', () => {
+		return result.then((data) => expect(data.results[0].warnings).toHaveLength(1));
+	});
 
-describe("flags warnings with invalid css", () => {
-  let result;
+	it('correct warning text', () => {
+		return result.then((data) =>
+			expect(data.results[0].warnings[0].text).toBe(
+				'Expected @if not statement rather than @if statement == null (scss/at-if-no-null)',
+			),
+		);
+	});
 
-  beforeEach(() => {
-    result = stylelint.lint({
-      code: invalidCss,
-      config
-    });
-  });
+	it('correct rule flagged', () => {
+		return result.then((data) =>
+			expect(data.results[0].warnings[0].rule).toBe('scss/at-if-no-null'),
+		);
+	});
 
-  it("did error", () => {
-    return result.then(data => expect(data.errored).toBeTruthy());
-  });
+	it('correct severity flagged', () => {
+		return result.then((data) => expect(data.results[0].warnings[0].severity).toBe('error'));
+	});
 
-  it("flags one warning", () => {
-    return result.then(data =>
-      expect(data.results[0].warnings).toHaveLength(1)
-    );
-  });
+	it('correct line number', () => {
+		return result.then((data) => expect(data.results[0].warnings[0].line).toBe(1));
+	});
 
-  it("correct warning text", () => {
-    return result.then(data =>
-      expect(data.results[0].warnings[0].text).toBe(
-        'Unexpected unknown type selector "madeup" (selector-type-no-unknown)'
-      )
-    );
-  });
-
-  it("correct rule flagged", () => {
-    return result.then(data =>
-      expect(data.results[0].warnings[0].rule).toBe("selector-type-no-unknown")
-    );
-  });
-
-  it("correct severity flagged", () => {
-    return result.then(data =>
-      expect(data.results[0].warnings[0].severity).toBe("error")
-    );
-  });
-
-  it("correct line number", () => {
-    return result.then(data =>
-      expect(data.results[0].warnings[0].line).toBe(1)
-    );
-  });
-
-  it("correct column number", () => {
-    return result.then(data =>
-      expect(data.results[0].warnings[0].column).toBe(1)
-    );
-  });
+	it('correct column number', () => {
+		return result.then((data) => expect(data.results[0].warnings[0].column).toBe(1));
+	});
 });
